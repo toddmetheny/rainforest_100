0e96216ec5769da07988b6f0981b1987ec9dcd2a
Todd Metheny <todd.metheny@gmail.com>
2014-11-18 17:45:15 -0500

    set default review to associate with product on page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :reviews
 
-  resources :products
+  resources :products do
+    resources :reviews
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
