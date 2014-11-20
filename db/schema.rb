a99c7426b9432d2a78d9fc77346dfe82afc1dc2a
Todd Metheny <todd.metheny@gmail.com>
2014-11-20 15:07:39 -0500

    added user column to reviews, updated views added relationship, now associates a review with a user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141120085635) do
+ActiveRecord::Schema.define(version: 20141120195046) do
 
   create_table "photos", force: true do |t|
     t.string   "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20141120085635) do
     t.integer  "price_in_cents"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
     t.string   "pic"
   end
 
@@ -53,9 +54,11 @@ ActiveRecord::Schema.define(version: 20141120085635) do
     t.integer  "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "reviews", ["product_id"], name: "index_reviews_on_product_id"
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
