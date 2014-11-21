612bf5524d22b608e50ac6024a7062c40c032a52
Todd Metheny <todd.metheny@gmail.com>
2014-11-21 04:55:39 -0500

    styling, jquery, added a couple gems

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+// $(document).ready(function(){
+// 	$(".product-image").on('mouseenter', function(){
+// 		$(this).closest("img").attr('src', 'assets/yoda.png');
+// 	}).on('mouseleave', function(){
+
+// 	});
+// 	// $('.product-image').hover(function(){
+// 	// 	$(this).closest().
+// 	// });
+// });
